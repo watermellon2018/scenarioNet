22c2d27971ca30ebe027f6fba895f7642e3767be
Kate <stepanovaks99@mail.ru>
2021-07-28 17:18:55 +0500

    Темная тема

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Button } from '@material-ui/core';
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/globalStyles";
+import { lightTheme, darkTheme } from "./components/Themes"
 
 function App() {
+
+  const [theme, setTheme] = useState('light');
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
+
   return (
-    <div className="App">
-     
-    </div>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles />
+        <div className="App">
+          <Button onClick={themeToggler} variant="contained">Default</Button>
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
 
